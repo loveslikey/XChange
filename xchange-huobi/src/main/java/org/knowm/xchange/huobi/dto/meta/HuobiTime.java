51c9f03545e4606415c3c6f7ed2856a68c6a8ba9
loveslikey <327802151@qq.com>
2018-02-22 16:07:54 +0800

    测试

---
+++ xchange-huobi/src/main/java/org/knowm/xchange/huobi/dto/meta/HuobiTime.java
@@ -0,0 +1,19 @@
+package org.knowm.xchange.huobi.dto.meta;
+
+import java.util.Date;
+
+/**
+ * Created by Administrator on 2018/2/19.
+ */
+public class HuobiTime {
+      private String  status;
+      private Date data;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
